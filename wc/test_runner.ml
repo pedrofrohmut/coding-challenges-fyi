39da7fb2af5a8beba4e141143df38d105fd81d64
pedrofrohmut <pedrofrohmut@gmail.com>
2026-08-16 19:17:05 -0300

    Test runner file. Testing help, version and count bytes

---
+++ wc/test_runner.ml
@@ -0,0 +1,63 @@
+#!/usr/bin/env utop
+
+#directory "+unix";;
+
+let rec match_output_lines (a_lines: string list) (b_lines: string list): unit =
+  match a_lines, b_lines with
+  | [], [] -> ()
+
+  | [], _ | _, [] -> failwith "Unmatch: Output have different number of lines"
+
+  | a :: at, b :: bt ->
+     if a = b then
+       match_output_lines at bt
+     else
+       begin
+         prerr_endline @@ Printf.sprintf "Lines are different. \n\twc: `%s` \n\tmy: `%s`\n" a b;
+         failwith "Unmatched lines in outputs"
+       end
+;;
+
+let e2e_test (cmd_a: string) (cmd_b: string): unit =
+  let (in_chan, out_chan) = Unix.open_process cmd_a in
+  close_out out_chan;
+  let wc_lines = In_channel.input_lines in_chan in
+  close_in in_chan;
+  let _ = Unix.wait () in
+
+  let (in_chan, out_chan) = Unix.open_process cmd_b in
+  close_out out_chan;
+  let my_lines = In_channel.input_lines in_chan in
+  close_in in_chan;
+  let _ = Unix.wait () in
+
+  let wc_len = List.length wc_lines in
+  let my_len = List.length my_lines in
+  if wc_len <> my_len then
+    failwith @@
+      Printf.sprintf
+        "Original tool and your tool output have different lengths.\n \tExpected: %d, but got: %d instead.\n"
+        wc_len
+        my_len
+  else
+    match_output_lines wc_lines my_lines
+;;
+
+let test_help (my_cmd: string): unit =
+  e2e_test "wc --help" (my_cmd ^ " --help")
+;;
+
+let test_version (my_cmd: string): unit =
+  e2e_test "wc --version" (my_cmd ^ " --version")
+;;
+
+let test_wc_count_bytes (my_cmd: string): unit =
+  e2e_test "wc --bytes ./man_wc.txt" (my_cmd ^ " --bytes ./man_wc.txt")
+;;
+
+let () =
+  let my_wc = "./_build/default/wc.exe" in
+  test_help my_wc;
+  test_version my_wc;
+  test_wc_count_bytes my_wc
+;;
