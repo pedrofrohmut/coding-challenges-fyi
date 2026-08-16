c7a29f2e78fd94284893f617a9ef4298b4de7bc4
pedrofrohmut <pedrofrohmut@gmail.com>
2026-08-15 22:08:43 -0300

    Test version flag

--- wc/sandbox.ml
+++ wc/sandbox.ml
@@ -44,5 +44,34 @@ let test_help () =
       ) wc_lines my_lines
 ;;
 
+let test_version () =
+  let (in_chan, out_chan) = Unix.open_process "wc --version" in
+  close_out out_chan;
+  let wc_lines = In_channel.input_lines in_chan in
+  close_in in_chan;
+  let _ = Unix.wait () in
+
+  let (in_chan, out_chan) = Unix.open_process (my_wc ^ " --version") in
+  close_out out_chan;
+  let my_lines = In_channel.input_lines in_chan in
+  close_in in_chan;
+  let _ = Unix.wait () in
+
+  let wc_len = List.length wc_lines in
+  let my_len = List.length my_lines in
+  if wc_len <> my_len then
+    begin
+      Printf.printf "Wc_lines length: %d, my_lines length: %d\n" wc_len my_len;
+      Printf.printf "Original tool output and your tool output have different lengths\n"
+    end
+  else
+    List.iter2 (fun a b ->
+        if a = b then () else
+          Printf.printf "Lines are different. \n\twc: `%s` \n\tmy: `%s`\n" a b
+      ) wc_lines my_lines
+;;
+
 let () =
-  test_help ()
+  test_help ();
+  test_version ()
+;;
