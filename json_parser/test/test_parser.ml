d62803c0187f96ef7724302901523665d252219c
Pedro Frohmut <pedrofrohmut@gmail.com>
2026-09-03 14:59:02 -0300

    All parser tests for step2

--- json_parser/test/test_parser.ml
+++ json_parser/test/test_parser.ml
@@ -4,7 +4,6 @@ open Printf
 
 let test_parser_step1_valid () =
   let input = get_input_from_file "test/json_inputs/step1/valid.json" in
-  (* printf "Input -> `%s`\n" input; *)
 
   let result = parse_input input in
 
@@ -15,7 +14,6 @@ let test_parser_step1_valid () =
 
 let test_parser_step1_invalid () =
   let input = get_input_from_file "test/json_inputs/step1/invalid.json" in
-  (* printf "Input -> `%s`\n" input; *)
 
   let result = parse_input input in
 
@@ -26,7 +24,6 @@ let test_parser_step1_invalid () =
 
 let test_parser_step2_valid () =
   let input = get_input_from_file "test/json_inputs/step2/valid.json" in
-  (* printf "Input -> `%s`\n" input; *)
 
   let result = parse_input input in
 
@@ -37,7 +34,6 @@ let test_parser_step2_valid () =
 
 let test_parser_step2_valid2 () =
   let input = get_input_from_file "test/json_inputs/step2/valid2.json" in
-  (* printf "Input -> `%s`\n" input; *)
 
   let result = parse_input input in
 
@@ -46,9 +42,8 @@ let test_parser_step2_valid2 () =
     exit 1
   )
 
-let test_parser_step2_valid2 () =
+let test_parser_step2_valid3 () =
   let input = get_input_from_file "test/json_inputs/step2/valid3.json" in
-  (* printf "Input -> `%s`\n" input; *)
 
   let result = parse_input input in
 
@@ -57,9 +52,32 @@ let test_parser_step2_valid2 () =
     exit 1
   )
 
+let test_parser_step2_invalid () =
+  let input = get_input_from_file "test/json_inputs/step2/invalid.json" in
+
+  let result = parse_input input in
+
+  if not result then () else (
+    print_endline "✗ FAIL: Failed step2 invalid";
+    exit 1
+  )
+
+let test_parser_step2_invalid2 () =
+  let input = get_input_from_file "test/json_inputs/step2/invalid2.json" in
+
+  let result = parse_input input in
+
+  if not result then () else (
+    print_endline "✗ FAIL: Failed step2 invalid2";
+    exit 1
+  )
+
 let run () =
   test_parser_step1_valid ();
   test_parser_step1_invalid ();
   test_parser_step2_valid ();
   test_parser_step2_valid2 ();
+  test_parser_step2_valid3 ();
+  test_parser_step2_invalid ();
+  test_parser_step2_invalid2 ();
   print_endline "✓ SUCCESS: All Parser tests passed."
